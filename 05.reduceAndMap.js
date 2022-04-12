9bb476157a29191f280c387511b09da631d7d3c2
AvetBadalyan <avetbadalyan@gmail.com>
2022-04-12 17:52:09 +0400

    feat: array reduce method implementation

--- 05.reduceAndMap.js
+++ 05.reduceAndMap.js
@@ -1,8 +1,9 @@
 // Implementation of Array methods REDUCE and MAP
+// NEED TO FIX UNDEFINED IN REDUCE
 
-// Array.map()
+// Array.map() method implementation
 
-Array.prototype.fakeMap = function (callback) {
+Array.prototype.fakeMap = (callback) => {
   const newArray = [];
 
   for (let i = 0; i < this.length; i++) {
@@ -16,3 +17,26 @@ let array = ["Avet", "Rafo", "Martun"];
 
 console.log(array.fakeMap((item) => "Senior " + item));
 console.log(array.map((item) => "Senior " + item));
+
+// Array.reduce() method implementation
+
+Array.prototype.fakeReduce = (callback, initialValue) => {
+  let acc = initialValue;
+  let current = 0;
+
+  if (!initialValue) {
+    acc = this[0];
+    current = 1;
+  } 
+
+  for (let i = current; i < this.length; i++) {
+    acc = callback(acc, this[i], i,this);
+  }
+
+  return acc;
+};
+
+const numbers = [2, 4, 6, 8, 10];
+
+console.log(numbers.reduce((acc, current) => acc + current, 0));
+console.log(numbers.fakeReduce((acc, current) => acc + current, 0));
